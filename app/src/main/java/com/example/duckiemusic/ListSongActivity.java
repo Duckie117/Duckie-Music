874a7b4d4be24cf6503b4e17723e95ea203a65a5
Duckie <ngotheduc117@gmail.com>
2024-01-26 00:40:29 +0700

    up login/register

---
+++ app/src/main/java/com/example/duckiemusic/ListSongActivity.java
@@ -0,0 +1,14 @@
+package com.example.duckiemusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListSongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_song);
+    }
+}
